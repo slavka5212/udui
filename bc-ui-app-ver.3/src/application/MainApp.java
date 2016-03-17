acc12b1516ff5ee7e9e5530711ef3d70b754c048
Slavka Ivanicova <slavka.ivanicova@gmail.com>
2016-03-17 13:50:08 +0100

    project

---
+++ bc-ui-app-ver.3/src/application/MainApp.java
@@ -0,0 +1,74 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import aima.gui.applications.AimaDemoApp;
+
+public class MainApp extends Application {
+	private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("UIApp");
+
+        
+        initRootLayout();
+
+        showVisualViews();
+    }
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the visual views inside the root layout.
+     */
+    public void showVisualViews() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/VisualViews.fxml"));
+            AnchorPane visualViews = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(visualViews);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
